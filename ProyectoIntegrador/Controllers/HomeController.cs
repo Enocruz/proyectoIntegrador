fdc2a2460302981896730d59ee64aafa961aa663
Oscar Ruiz <a01376200@itesm.mx>
2018-05-04 18:06:38 -0500

    Compresión y manejo de errores agregado

--- ProyectoIntegrador/Controllers/HomeController.cs
+++ ProyectoIntegrador/Controllers/HomeController.cs
@@ -1,9 +1,15 @@
 ﻿using System.Web.Mvc;
+using System;
+using System.IO.Compression;
+using System.Drawing;
+using System.Collections.Generic;
 
 namespace ProyectoIntegrador.Controllers
 {
     public class HomeController : Controller
     {
+        string[] errors = { "La imagen debe estar en escala de grises", "Debes seleccionar al menos una región" };
+        
         public ActionResult Index()
         {
             return View();
@@ -11,6 +17,34 @@ namespace ProyectoIntegrador.Controllers
 
         public ActionResult Impresion()
         {
+            if (Request.RequestType.Equals("POST"))
+            {
+                var regiones = Request.Form.AllKeys;
+                if (regiones.Length <= 0)
+                {
+                    Response.Redirect("/Home/Impresion?error=1");
+                    return View();
+                }
+                System.Diagnostics.Debug.WriteLine(regiones);
+                using (ZipArchive zip = new ZipArchive(Request.Files.Get("image").InputStream))
+                {
+                    foreach (ZipArchiveEntry entry in zip.Entries)
+                    {
+                        var img = Bitmap.FromStream(entry.Open());
+                        if(!TestGrayScale(new Bitmap(img), 8))
+                        {
+                            Response.Redirect("/Home/Impresion?error=0");
+                            return View();
+                        }
+                        System.Diagnostics.Debug.WriteLine(new Bitmap(img).HorizontalResolution);
+                    }
+                }
+                Response.Redirect("/Home/Detalle");
+            }
+            if(Request.QueryString["error"] != null)
+            {
+                ViewBag.Error = errors[Int32.Parse(Request.QueryString["error"])];
+            }
             ViewBag.Header = "Identificación de huellas de impresión";
             return View();
         }
@@ -39,6 +73,23 @@ namespace ProyectoIntegrador.Controllers
         {
             ViewBag.Message = "Detalle de identificación";
             return View();
+        }
+
+        public bool TestGrayScale(Bitmap bmp, int threshold)
+        {
+            Color pixelColor = Color.Empty;
+            int rgbDelta;
+
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    pixelColor = bmp.GetPixel(x, y);
+                    rgbDelta = Math.Abs(pixelColor.R - pixelColor.G) + Math.Abs(pixelColor.G - pixelColor.B) + Math.Abs(pixelColor.B - pixelColor.R);
+                    if (rgbDelta > threshold) return false;
+                }
+            }
+            return true;
         }
     }
 }
