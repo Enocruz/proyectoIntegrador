40371a022fd056b55a754bbc39fd8d6b25e09699
Brandon Cruz <brandon.cruz@konfio.mx>
2018-05-04 18:44:13 -0500

    WIP

---
+++ ProyectoIntegrador/Controllers/HistorialController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoIntegrador.Models;
+
+namespace ProyectoIntegrador.Controllers
+{
+    public class HistorialController : Controller
+    {
+        private Modulo_IdentificacionEntities db = new Modulo_IdentificacionEntities();
+
+        // GET: Historial
+        public ActionResult Index()
+        {
+            var hISTORIAL = db.HISTORIAL.Include(h => h.MOVIMIENTO).Include(h => h.SUBCASO).Include(h => h.USUARIO);
+            return View(hISTORIAL.ToList());
+        }
+
+        // GET: Historial/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HISTORIAL hISTORIAL = db.HISTORIAL.Find(id);
+            if (hISTORIAL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hISTORIAL);
+        }
+
+        // GET: Historial/Create
+        public ActionResult Create()
+        {
+            ViewBag.idMovimiento = new SelectList(db.MOVIMIENTO, "idMovimiento", "Descripcion");
+            ViewBag.idSubCaso = new SelectList(db.SUBCASO, "idSubcaso", "DESCRIPCION");
+            ViewBag.idUsuario = new SelectList(db.USUARIO, "idUsuario", "Apellido");
+            return View();
+        }
+
+        // POST: Historial/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idHistorial,idUsuario,idSubCaso,idMovimiento,fecha")] HISTORIAL hISTORIAL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HISTORIAL.Add(hISTORIAL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idMovimiento = new SelectList(db.MOVIMIENTO, "idMovimiento", "Descripcion", hISTORIAL.idMovimiento);
+            ViewBag.idSubCaso = new SelectList(db.SUBCASO, "idSubcaso", "DESCRIPCION", hISTORIAL.idSubCaso);
+            ViewBag.idUsuario = new SelectList(db.USUARIO, "idUsuario", "Apellido", hISTORIAL.idUsuario);
+            return View(hISTORIAL);
+        }
+
+        // GET: Historial/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HISTORIAL hISTORIAL = db.HISTORIAL.Find(id);
+            if (hISTORIAL == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idMovimiento = new SelectList(db.MOVIMIENTO, "idMovimiento", "Descripcion", hISTORIAL.idMovimiento);
+            ViewBag.idSubCaso = new SelectList(db.SUBCASO, "idSubcaso", "DESCRIPCION", hISTORIAL.idSubCaso);
+            ViewBag.idUsuario = new SelectList(db.USUARIO, "idUsuario", "Apellido", hISTORIAL.idUsuario);
+            return View(hISTORIAL);
+        }
+
+        // POST: Historial/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idHistorial,idUsuario,idSubCaso,idMovimiento,fecha")] HISTORIAL hISTORIAL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hISTORIAL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idMovimiento = new SelectList(db.MOVIMIENTO, "idMovimiento", "Descripcion", hISTORIAL.idMovimiento);
+            ViewBag.idSubCaso = new SelectList(db.SUBCASO, "idSubcaso", "DESCRIPCION", hISTORIAL.idSubCaso);
+            ViewBag.idUsuario = new SelectList(db.USUARIO, "idUsuario", "Apellido", hISTORIAL.idUsuario);
+            return View(hISTORIAL);
+        }
+
+        // GET: Historial/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HISTORIAL hISTORIAL = db.HISTORIAL.Find(id);
+            if (hISTORIAL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hISTORIAL);
+        }
+
+        // POST: Historial/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HISTORIAL hISTORIAL = db.HISTORIAL.Find(id);
+            db.HISTORIAL.Remove(hISTORIAL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoIntegrador/Controllers/UsuarioController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoIntegrador.Models;
+
+namespace ProyectoIntegrador.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private Modulo_IdentificacionEntities db = new Modulo_IdentificacionEntities();
+
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            var uSUARIO = db.USUARIO.Include(u => u.LICENCIA);
+            return View(uSUARIO.ToList());
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            ViewBag.idLicencia = new SelectList(db.LICENCIA, "idLicencia", "Nombre");
+            return View();
+        }
+
+        // POST: Usuario/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idUsuario,Apellido,Nombre,Organizacion,Creditos,Fecha_nacimiento,idLicencia,Inicio_Licencia")] USUARIO uSUARIO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.USUARIO.Add(uSUARIO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idLicencia = new SelectList(db.LICENCIA, "idLicencia", "Nombre", uSUARIO.idLicencia);
+            return View(uSUARIO);
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idLicencia = new SelectList(db.LICENCIA, "idLicencia", "Nombre", uSUARIO.idLicencia);
+            return View(uSUARIO);
+        }
+
+        // POST: Usuario/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idUsuario,Apellido,Nombre,Organizacion,Creditos,Fecha_nacimiento,idLicencia,Inicio_Licencia")] USUARIO uSUARIO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uSUARIO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idLicencia = new SelectList(db.LICENCIA, "idLicencia", "Nombre", uSUARIO.idLicencia);
+            return View(uSUARIO);
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            if (uSUARIO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uSUARIO);
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            USUARIO uSUARIO = db.USUARIO.Find(id);
+            db.USUARIO.Remove(uSUARIO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
